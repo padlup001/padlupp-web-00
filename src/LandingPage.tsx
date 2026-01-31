90870193690c00339bc062e337767cc6233216f7
BrainOnSteroids <63648245+khal33d-hub@users.noreply.github.com>
2026-01-31 18:51:52 +0000

    changed countdown
    changed countdown to 1st of march

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -57,7 +57,7 @@ export const LandingPage: FC = () => {
   const resumeTimeoutRef = useRef<number | null>(null);
   const pausedRef = useRef(false);
   const currentYear = new Date().getUTCFullYear();
-  const launchTarget = useRef<Date>(new Date(Date.UTC(currentYear, 1, 1, 0, 0, 0)));
+  const launchTarget = useRef<Date>(new Date(Date.UTC(currentYear, 2, 1, 0, 0, 0)));
 
   // Move this here, top level (not inside if/blocks)
   const handleScrollToFooter = () => {
